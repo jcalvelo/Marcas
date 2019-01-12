e27d18559e07bba8bfaf244cdc74a7627346ac72
José Calvelo <j.calvelo1993@gmail.com>
2019-01-11 21:58:46 -0300

    Formato de archivos

--- MarcasServidor/IMarcas.cs
+++ MarcasServidor/IMarcas.cs
@@ -5,12 +5,10 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-namespace MarcasServidor
-{
+namespace MarcasServidor {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMarcaEntrada" in both code and config file together.
     [ServiceContract]
-    public interface IMarcas
-    {
+    public interface IMarcas {
         [OperationContract]
         void MarcaEntrada(String user, String login_computer);
         [OperationContract]
--- MarcasServidor/Marcas.cs
+++ MarcasServidor/Marcas.cs
@@ -6,48 +6,34 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-namespace MarcasServidor
-{
-    public class Marcas : IMarcas
-    {
-        
-        public void Start() {
+namespace MarcasServidor {
+    public class Marcas : IMarcas {
 
-        }
-
-        public void MarcaEntrada(String user, String login_computer)
-        {
+        public void MarcaEntrada(String user, String login_computer) {
             DateTime now = DateTime.Now;
             String path = @"C:\Datos\MarcasPresentismo\" + DateTime.Now.ToString("MMMM") + "-" + now.Year + ".csv";
 
             if (File.Exists(path)) {
-                using (StreamWriter file = new StreamWriter(path, true))
-                {
+                using (StreamWriter file = new StreamWriter(path, true)) {
                     file.WriteLine(now.ToShortDateString() + ";" + now.ToShortTimeString() + ";" + user + ";" + login_computer + ";" + "INICIO");
                 }
-            } else
-            {
+            } else {
                 String line = now.ToShortDateString() + ";" + now.ToShortTimeString() + ";" + user + ";" + login_computer + ";" + "INICIO" + Environment.NewLine;
                 File.WriteAllText(path, line);
             }
-            
+
         }
 
-        public void MarcaSalida(String user, String login_computer)
-        {
+        public void MarcaSalida(String user, String login_computer) {
             DateTime now = DateTime.Now;
             String path = @"C:\Datos\MarcasPresentismo\" + DateTime.Now.ToString("MMMM") + "-" + now.Year + ".csv";
 
 
-            if (File.Exists(path))
-            {
-                using (StreamWriter file = new StreamWriter(path, true))
-                {
+            if (File.Exists(path)) {
+                using (StreamWriter file = new StreamWriter(path, true)) {
                     file.WriteLine(now.ToShortDateString() + ";" + now.ToShortTimeString() + ";" + user + ";" + login_computer + ";" + "CIERRE");
                 }
-            } 
-            else
-            {
+            } else {
                 String line = now.ToShortDateString() + ";" + now.ToShortTimeString() + ";" + user + ";" + login_computer + ";" + "CIERRE" + Environment.NewLine;
                 File.WriteAllText(path, line);
             }
